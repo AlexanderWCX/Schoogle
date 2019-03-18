6e52ae63b9bda487edec2fa7809ea0e6008acf9c
Germaine Kwek <danyi1998@gmail.com>
2019-03-18 11:37:27 +0800

    germ's changes
    getDistance, read_db and write_existing db code are found in sortingAndRelatedFunctions(sorting) and saveRemoveSchool(user saves and unsaves a school)

---
+++ saveRemoveSchool.py
@@ -0,0 +1,145 @@
+# import relevant libraries
+import xlrd
+from xlutils.copy import copy
+
+
+# save school
+def saveSchool(username, schoolName, savedDate):
+
+    # make a copy of the workbook and make changes to it
+    wb = xlrd.open_workbook('userInformation.xls')
+    ws = wb.sheet_by_name('userInformation')
+    newWB = copy(wb)
+    newWS = newWB.get_sheet(0)
+
+    # retrieve the number of records stored in the userInformation database
+    numOfRecords = int(ws.cell(0, 1).value)
+
+    # records start at row index 2(refer to the userInformation database layout)
+    # initialize row
+    row = 2
+
+    # loop through each record in the userInformation database
+    for i in range(numOfRecords):
+
+        # check if the username matches that passed as an argument into this function
+        if username == ws.cell(row, 0).value:
+            # retrieve number of schools stored in that record
+            numOfSchools = int(ws.cell(row + 1, 0).value)
+
+            # increment the number of schools saved
+            numOfSchools += 1
+
+            # write the new number of schools saved to the record
+            newWS.write(row + 1, 0, numOfSchools)
+
+            # at the matching column index, write the new school and its saved date
+            newWS.write(row + 1, numOfSchools, schoolName)
+            newWS.write(row + 2, numOfSchools, savedDate)
+
+            # move one cell to the right and write 'end' to it
+            newWS.write(row + 1, numOfSchools + 1, 'end')
+            newWS.write(row + 2, numOfSchools + 1, 'end')
+
+            newWB.save('userInformation.xls')
+
+            return
+
+
+# delete school
+def deleteSavedSchool(username, schoolName):
+
+    # make a copy of the workbook and make changes to it
+    wb = xlrd.open_workbook('userInformation.xls')
+    ws = wb.sheet_by_name('userInformation')
+    newWB = copy(wb)
+    newWS = newWB.get_sheet(0)
+
+    # retrieve the number of records stored in the userInformation database
+    numOfRecords = int(ws.cell(0, 1).value)
+
+    # records start at row index 2(refer to the userInformation database layout)
+    # initialize row
+    row = 2
+
+    # loop through each record in the userInformation database
+    for i in range(numOfRecords):
+
+        # check if the username matches that passed as an argument into this function
+        if username == ws.cell(row, 0).value:
+
+            # retrieve number of schools stored in that record
+            numOfSchools = int(ws.cell(row + 1, 0).value)
+
+            # decrement the number of schools saved
+            numOfSchools -= 1
+
+            # write the new number of schools saved to the record
+            newWS.write(row + 1, 0, numOfSchools)
+
+            # initialize col
+            col = 1
+
+            # initialize sn
+            sn = ws.cell(row + 1, col).value
+
+            # while we have not reached 'end'
+            while (sn != 'end'):
+
+                # compare sn with schoolName
+                if sn == schoolName:
+                    break
+
+                else:
+                    col += 1
+                    sn = ws.cell(row + 1, col).value
+
+
+            # if sn == 'end'
+            if (sn == 'end'):
+                print('The school is not saved!')
+                return
+
+            # increment col
+            col += 1
+
+            # get the next sn
+            sn = ws.cell(row + 1, col).value
+            sd = ws.cell(row + 2, col).value
+
+            # loop through the remaining cells till 'end' and shift them left by one column each
+            while (sn != 'end'):
+
+                # shift the school name left by one cell
+                newWS.write(row + 1, col - 1, sn)
+
+                # shift the saved date left by one cell
+                newWS.write(row + 2, col - 1, sd)
+
+                # move on to the next school
+                col += 1
+                sn = ws.cell(row + 1, col).value
+                sd = ws.cell(row + 2, col).value
+
+            # shift 'end' left by one cell
+            newWS.write(row + 1, col - 1, 'end')
+            newWS.write(row + 2, col - 1, 'end')
+            newWS.write(row + 1, col, '')
+            newWS.write(row + 2, col, '')
+
+            newWB.save('userInformation.xls')
+
+            return
+
+        else:
+            row += 4
+
+
+deleteSavedSchool('username 2', 'school 3')
+
+
+
+
+
+
+
